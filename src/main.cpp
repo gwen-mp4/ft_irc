3ffcbe55e2aad7bdae9911f5c7bd600f55db39b2
gwen <gwen@student.42.fr>
2026-05-29 15:20:43 +0200

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gwen <gwen@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 11:51:34 by gwen              #+#    #+#             */
-/*   Updated: 2026/05/29 11:58:47 by gwen             ###   ########.fr       */
+/*   Updated: 2026/05/29 15:19:28 by gwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 int	mian(int ac, char **av) {
 	if (ac != 3)
-		return std::cerr << "No.\n", 1;
+		return std::cerr << "Usage: ./ircserv <port> <password>\n", 1;
 	try {
+		//parsing
 		Server	srv;
 		srv.run();
 	} catch (const std::exception& e) {
