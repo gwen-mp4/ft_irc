f1244d7ed0784de55f3bc748bc8fb4392021b4c6
gwen <gwen@student.42.fr>
2026-05-29 11:31:04 +0200

    setting files

---
+++ inc/Include.hpp
@@ -0,0 +1,15 @@
+#ifndef INCLUDE_HPP
+#define INCLUDE_HPP
+
+#include <iostream>
+#include <vector>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <csignal>
+
+#endif
