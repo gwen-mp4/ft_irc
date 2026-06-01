05f6c22bc6c99d4cd46f96c1cbeb4ce7d5798b43
gwen <gwen@student.42.fr>
2026-06-01 15:26:30 +0200

    parse okay

--- inc/Command.hpp
+++ inc/Command.hpp
@@ -6,7 +6,7 @@
 /*   By: gwen <gwen@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 11:16:12 by gwen              #+#    #+#             */
-/*   Updated: 2026/06/01 14:00:49 by gwen             ###   ########.fr       */
+/*   Updated: 2026/06/01 15:10:21 by gwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ class Command {
 		Command& operator=(const Command& other);
 		~Command();
 
+		std::string	getPrefix() const;
+		std::string	getCommand() const;
+		std::vector<std::string>	getParams() const;
 };
 
 #endif
