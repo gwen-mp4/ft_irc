efdd96387a99eb71878379bab4b6bcb42c782c24
storck <storck@f4r4s5.paris.42.school>
2026-06-04 11:07:35 +0200

    did correct sendClientMessage()

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: storck <storck@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 11:52:08 by gwen              #+#    #+#             */
-/*   Updated: 2026/06/03 17:37:46 by storck           ###   ########.fr       */
+/*   Updated: 2026/06/04 11:05:01 by storck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,13 @@ Server::~Server( void )
 // // Send a message to client with the code (defined in ServerCodeIRC.hpp)
 void    Server::sendClientMessage(int clientFD, std::string message)
 {
-    treatCommand(this->_clients[clientFD], message);
+    std::string p1 = clientFD + "0";
+    std::string p2 = "\033[96mClient: " + message + "\033[m";
+
+    if (send(clientFD, p2.c_str(), p2.size(), 0) == -1)
+        std::cerr << "send() failed." << std::endl;
+    //send(clientFD, message.c_str(), message.size(), 0);
+    //treatCommand(this->_clients[clientFD], message);
 }
 
 
